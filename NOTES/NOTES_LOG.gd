e9635700b8853c67a03dc168a5f7b700e1b4103e
LiLFishowo <gamerlife640@gmail.com>
2026-05-07 15:43:47 -0400

    5/7/26
    trying to figure out how to edit and use tilemaps

--- NOTES/NOTES_LOG.gd
+++ NOTES/NOTES_LOG.gd
@@ -9,11 +9,14 @@ extends Node
 # DO NOT FILL WITH NONSENSE THIS IS SO YOU CAN UNDERSTAND WHAT YOU ARE DOING 
 #  AND WHERE YOU LEFT OFF LAST
 
-# 5/4/29
-# GOALS 
-# Charcter controller
-# a good viewport
-# start enemy build (look up enemy AI)
+# you will be dating and posting finshed work here
+
+# 5/7/26 
+# connected with github
+# setting up tileset + map 
+#
+
+
 
 
 
